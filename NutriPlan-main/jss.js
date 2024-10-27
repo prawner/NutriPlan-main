3d63f0315f71b1e30e40ae720a9ab51e5cc04acc
prawner <alvintandiarrangsumual@gmail.com>
2024-10-27 16:41:57 +0700

    detailPage

---
+++ NutriPlan-main/jss.js
@@ -0,0 +1,13 @@
+document.getElementById("breakfast-toggle").addEventListener("click", function () {
+  const menu = document.getElementById("breakfast-menu");
+  menu.style.display = menu.style.display === "none" || menu.style.display === "" ? "block" : "none";
+});
+document.getElementById("breakfast-toggle-2").addEventListener("click", function () {
+  const menu = document.getElementById("breakfast-menu-2");
+  menu.style.display = menu.style.display === "none" || menu.style.display === "" ? "block" : "none";
+});
+document.getElementById("breakfast-toggle-3").addEventListener("click", function () {
+  const menu = document.getElementById("breakfast-menu-3");
+  menu.style.display = menu.style.display === "none" || menu.style.display === "" ? "block" : "none";
+});
+
